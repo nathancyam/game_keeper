25738955d150135b78c880f743885f02ee27eb13
Nathan Yam <nathancyam@gmail.com>
2026-02-28 22:26:26 +1100

    Get random producer

--- lib/game_keeper/events.ex
+++ lib/game_keeper/events.ex
@@ -3,9 +3,12 @@ defmodule GameKeeper.Events do
 
   alias GameKeeper.Events.Pipeline
 
-  def push_and_ack_events(game_id, events)
-      when is_binary(game_id) and is_list(events) do
-    [producer] = Broadway.producer_names(Pipeline)
+  def push_and_ack_events(game_id, events) when is_binary(game_id) and is_list(events) do
+    producer =
+      Pipeline
+      |> Broadway.producer_names()
+      |> Enum.random()
+
     GenStage.call(producer, {:push_events, game_id, events})
   end
 end
