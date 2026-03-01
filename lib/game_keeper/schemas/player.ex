288bb9f8f985cfefdd80ea40544f495a03acc231
Nathan Yam <nathancyam@gmail.com>
2026-03-01 21:22:32 +1100

    Add players and teams

---
+++ lib/game_keeper/schemas/player.ex
@@ -0,0 +1,20 @@
+defmodule GameKeeper.Schemas.Player do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "players" do
+    field :name, :string
+    field :team_id, :binary_id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(player, attrs) do
+    player
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
